61af34a476d8ae9d4df08a459ec5a9f84555d41c
Hirsh Gupta <hirshgupta134@gmail.com>
2025-03-08 01:01:32 -0500

    update schedule switcher

--- databases/schedule_switcher.sql
+++ databases/schedule_switcher.sql
@@ -3,7 +3,7 @@ CREATE TABLE switcher (
     season INT,
     week INT,
     team VARCHAR(255),
-    schedul_of VARCHAR(255),
-    result DECIMAL(),
+    schedule_of VARCHAR(255),
+    result DECIMAL(2, 1),
     created timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
--- scripts/scenarios/update_scenarios.py
+++ scripts/scenarios/update_scenarios.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from scripts.utils import (utils as ut,
-                           constants as const)
-from scripts.scenarios.scenarios import (get_h2h, schedule_switcher)
 
+from scripts.utils import (utils as ut, constants as const)
+from scripts.scenarios.scenarios import (get_h2h, schedule_switcher)
 
 
 def commit_h2h(connection, h2h_data: pd.DataFrame):
@@ -33,12 +32,12 @@ def commit_ss(connection, ss_data: pd.DataFrame):
 
         query = '''
         INSERT INTO
-        h2h
-            (id, season, week, team, opp, result)
+        switcher
+            (id, season, week, team, schedule_of, result)
         VALUES
             (%s, %s, %s, %s, %s, %s);
         '''
-        values = (row.id, row.season, row.week, row.team, row.opp, row.win)
+        values = (row.id, row.season, row.week, row.team, row.schedule_of, row.result)
 
         c.execute(query, values)
         connection.commit()
@@ -46,18 +45,22 @@ def commit_ss(connection, ss_data: pd.DataFrame):
 
 
 def update_scenarios_db(season: int=const.SEASON,
-                     league_id: int=const.LEAGUE_ID,
-                     swid: str=const.SWID,
-                     espn_s2: str=const.ESPN_S2):
+                        league_id: int=const.LEAGUE_ID,
+                        swid: str=const.SWID,
+                        espn_s2: str=const.ESPN_S2):
 
     d = ut.load_data(league_id=league_id, swid=swid, espn_s2=espn_s2, season=season)
     params = ut.get_params(d)
+    week = params['current_week']
+    end = params['regular_season_end']
 
-    h2h_data = get_h2h(params=params, season=season, week=params['current_week'])
-    ss_data = schedule_switcher(params=params, season=season, week=params['current_week'])
-    with ut.mysql_connection() as conn:
-        commit_h2h(connection=conn, h2h_data=h2h_data)
-        commit_ss(connection=conn, ss_data=ss_data)
+    if week <= end:
+        h2h_data = get_h2h(params=params, season=season, week=week)
+        ss_data = schedule_switcher(params=params, season=season, week=week)
+        with ut.mysql_connection() as conn:
+            commit_h2h(connection=conn, h2h_data=h2h_data)
+            commit_ss(connection=conn, ss_data=ss_data)
+
 
 if __name__ == '__main__':
     update_scenarios_db()

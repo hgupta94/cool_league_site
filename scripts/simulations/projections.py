96fbf4f6197984c3bd82947e4f20da7453725855
Hirsh Gupta <hirshgupta134@gmail.com>
2025-03-16 12:03:09 -0400

    update projections db

--- scripts/simulations/projections.py
+++ scripts/simulations/projections.py
@@ -1,17 +1,19 @@
 from scripts.api.DataLoader import DataLoader
 from scripts.utils import constants as const
+import difflib
 import pandas as pd
 pd.options.mode.chained_assignment = None
-import difflib
 
 
 def match_player_to_espn(the_player: str,
-                         player_lookup: list):
+                         players: list):
+    player_lookup = [f"{p['player']['fullName']}|{const.NFL_TEAM_MAP[p['player']['proTeamId']]}" for p in players]
+
     calc = [difflib.SequenceMatcher(None, the_player, m).ratio() for m in player_lookup]
     if max(calc) > 0.8:
         match_idx = calc.index(max(calc))
-        # print(f'{the_player} --- {player_lookup[match_idx]} --- {max(calc)}')
-        return match_idx
+
+        return players[match_idx]['id']
     else:
         return None
 
@@ -20,7 +22,6 @@ def get_week_projections(week):
     """Return current week's projections for all positions"""
     data = DataLoader()
     players = data.players()['players']
-    player_lookup = [f"{p['player']['fullName']}|{const.NFL_TEAM_MAP[p['player']['proTeamId']]}" for p in players]
 
     positions = ['qb', 'rb', 'wr', 'te', 'dst']
 
@@ -28,6 +29,7 @@ def get_week_projections(week):
     for pos in positions:
         # print(pos)
         url = f"https://www.fantasypros.com/nfl/projections/{pos}.php?scoring=HALF&week={week}"
+        # url = f"https://www.fantasypros.com/nfl/rankings/half-point-ppr-{pos}.php?scoring=HALF&week={week}"
         df = pd.read_html(url)[0]
 
         # drop multi index column
@@ -67,27 +69,6 @@ def get_week_projections(week):
     projections['id'] = (projections.player.str.replace(' ', '')
                          + '_' + projections.season.astype(str)
                          + '_' + projections.week.astype(str).str.zfill(2))
-    projections['espn_id'] = projections.apply(lambda x: match_player_to_espn(x['match_on'], player_lookup), axis=1)
+    projections['espn_id'] = projections.apply(lambda x: match_player_to_espn(x['match_on'], players), axis=1)
 
     return projections[~projections.espn_id.isnull()]
-
-
-def commit_projections(connection,
-                       projections: pd.DataFrame):
-    print(f'Writing projections to database')
-
-    for idx, row in projections.iterrows():
-        print(f'Committing {row[0]}...', end='\n')
-        c = connection.cursor()
-        query = '''
-        INSERT INTO
-        projections
-            (id, season, week, name, espn_id, position, receptions, projection)
-        VALUES
-            (%s, %s, %s, %s, %s, %s, %s, %s);
-        '''
-        values = (row.id, row.season, row.week, row.player, row.espn_id, row.position, row.rec, row.fpts)
-
-        c.execute(query, values)
-        connection.commit()
-        print('Success!', end='\n')

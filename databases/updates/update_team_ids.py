3a05dc1c485b78af3d0f8c7f7ff894034128c161
Hirsh Gupta <hirshgupta134@gmail.com>
2026-08-16 22:22:09 -0400

    aligned update_team_ids with pythonanywhere

--- databases/updates/update_team_ids.py
+++ databases/updates/update_team_ids.py
@@ -1,13 +1,19 @@
 from scripts.api.dataloader import DataLoader
 from scripts.utils.database import Database
+from scripts.utils import constants
 
 
-def load_team_ids(dataloader: DataLoader, season: int) -> None:
+def update_team_ids(
+    season: int = constants.SEASON,
+    upsert: bool = False,
+    update_columns: list[str] | None = None
+    ):
+    # update every season after the draft
     rows = []
-    teams = dataloader.teams()['teams']
+    teams = dl.teams()['teams']
     for team in teams:
         rows.append((
-            f'{season}_{team['id']:02}',
+            f"{season}_{team['id']:02}",
             season,
             team['id'],
             team['primaryOwner']
@@ -17,6 +23,12 @@ def load_team_ids(dataloader: DataLoader, season: int) -> None:
         table='team_ids',
         columns='id, season, team_id, manager_id',
         rows=rows,
-        upsert=False,
-        update_columns=None
+        upsert=upsert,
+        update_columns=update_columns
     )
+
+
+if __name__ == '__main__':
+    season = constants.SEASON
+    dl = DataLoader(year=constants.SEASON)
+    update_team_ids()
